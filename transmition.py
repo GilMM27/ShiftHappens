c53c82b37513236d2963d37c7175947dc979428d
Gilberto Malagamba Montejo <A00839075@tec.mx>
2024-03-15 22:08:48 -0600

    velocidades funcionando mejor

--- transmition.py
+++ transmition.py
@@ -1,13 +1,11 @@
 import pygame
 import os
 import sys
+import math
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('CODICON')
 
-# prueba = pygame.image.load('prueba.png')
-# screen.blit(prueba, (0, 0))
-# pygame.display.update()
 class ImageDisplay:
     def __init__(self, screen):
         self.screen = screen
@@ -29,11 +27,13 @@ image_display = ImageDisplay(screen)
 image_display.show_image()
 
 # Car properties
-car_velocity = 0  # Initial velocity in km/h
-rpm = 0  # Initial RPM
-rpm_to_velocity_factor = 0.000005  # Conversion factor from RPM to velocity
-gear_ratios = [5, 3.5, 2, 1.5, 1.0, 0.7]  # Gear ratios for gears 1 to 5
-current_gear = 1  # Initial gear
+car_velocity = 0  # Velocidad inicial del automóvil en km/h
+current_gear = 0  # Marcha actual
+rpm = 0  # RPM inicial
+rpm_to_velocity_factor = 0.00007  # Factor de conversión de RPM a velocidad
+gear_ratios = [3.5, 3, 2.5, 2, 1.5, 1]  # Relaciones de marcha para las marchas 1 a 5
+MAX_SPEEDS = [60, 100, 140, 180, 220, 260]  # Velocidades máximas para cada marcha en km/h
+MIN_SPEEDS = [0, 40, 80, 120, 150, 180]  # Velocidades mínimas para cada marcha en km/h
 
 # Clock for controlling the frame rate
 clock = pygame.time.Clock()
@@ -42,11 +42,19 @@ run = True
 while run:
     if image_display.current_image_index < 6:
         current_gear = image_display.current_image_index
-        if rpm < 7000: rpm += 30
-        car_velocity += rpm * rpm_to_velocity_factor * gear_ratios[current_gear - 1]
+        if car_velocity < MIN_SPEEDS[current_gear]: print('No se puede cambiar a esta marcha')
+        else:
+            if rpm < 7000: rpm += 30
+            car_velocity += rpm * rpm_to_velocity_factor * gear_ratios[current_gear] * (1 - car_velocity / MAX_SPEEDS[current_gear])
+        time = 0
     else:
         rpm = 0
-    print('Gear:', current_gear, 'RPM:', rpm, 'Velocity:', car_velocity)
+        if car_velocity > 0:
+            # deceleration_factor = max(0.01, car_velocity / MAX_SPEEDS[current_gear])
+            car_velocity -= min(pow(time, 2), 0.05)
+            if car_velocity < 0: car_velocity = 0
+            time += 0.001
+    print('gear ',current_gear+1, 'RPM:', rpm, 'Velocity:', car_velocity)
 
     for event in pygame.event.get(): # Evento pa las keys
         if event.type == pygame.KEYDOWN: # Cuando se preciona una tecla
