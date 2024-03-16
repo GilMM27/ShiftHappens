e1557d1a43bda6aee174a50fb2d83b6a8d8b562d
Gilberto Malagamba Montejo <A00839075@tec.mx>
2024-03-15 22:46:18 -0600

    menu

---
+++ imageDisplay.py
@@ -0,0 +1,17 @@
+import pygame
+
+class ImageDisplay:
+    def __init__(self, screen):
+        self.screen = screen
+        self.images = ['assets/1ra.png', 'assets/2da.png', 'assets/3ra.png','assets/4ta.png','assets/5ta.png','assets/6ta.png','assets/n1.png','assets/n2.png','assets/n3.png']  # List of image filenames
+        self.current_image_index = 7
+
+    def show_image(self):
+        image = pygame.image.load(self.images[self.current_image_index])
+        self.screen.blit(image, (1280/2 - 263/2, 720/2 - 274/2))
+        pygame.display.flip()
+
+    def change_image(self, index):
+        if index >= 0 and index < len(self.images):
+            self.current_image_index = index
+            self.show_image()
--- transmition.py
+++ transmition.py
@@ -2,89 +2,117 @@ import pygame
 import os
 import sys
 import math
+from imageDisplay import ImageDisplay
+from button import Button
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('CODICON')
 
-class ImageDisplay:
-    def __init__(self, screen):
-        self.screen = screen
-        self.images = ['1ra.png', '2da.png', '3ra.png','4ta.png','5ta.png','6ta.png','n1.png','n2.png','n3.png']  # List of image filenames
-        self.current_image_index = 7
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
 
-    def show_image(self):
-        image = pygame.image.load(self.images[self.current_image_index])
-        self.screen.blit(image, (1280/2 - 263/2, 720/2 - 274/2))
-        pygame.display.flip()
+def main_menu():
+    while True:
+        screen.fill("#000000")
 
-    def change_image(self, index):
-        if index >= 0 and index < len(self.images):
-            self.current_image_index = index
-            self.show_image()
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-# Create an instance of the ImageDisplay class
-image_display = ImageDisplay(screen)
-image_display.show_image()
+        MENU_TEXT = get_font(85).render("SHIFT HAPPENS", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-# Car properties
-car_velocity = 0  # Velocidad inicial del automóvil en km/h
-current_gear = 0  # Marcha actual
-rpm = 0  # RPM inicial
-rpm_to_velocity_factor = 0.00007  # Factor de conversión de RPM a velocidad
-gear_ratios = [3.5, 3, 2.5, 2, 1.5, 1]  # Relaciones de marcha para las marchas 1 a 5
-MAX_SPEEDS = [60, 100, 140, 180, 220, 260]  # Velocidades máximas para cada marcha en km/h
-MIN_SPEEDS = [0, 40, 80, 120, 150, 180]  # Velocidades mínimas para cada marcha en km/h
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), text_input="PLAY", font=get_font(60), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), text_input="OPTIONS", font=get_font(60), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), text_input="QUIT", font=get_font(60), base_color="#d7fcd4", hovering_color="White")
 
-# Clock for controlling the frame rate
-clock = pygame.time.Clock()
+        screen.blit(MENU_TEXT, MENU_RECT)
 
-run = True
-while run:
-    if image_display.current_image_index < 6:
-        current_gear = image_display.current_image_index
-        if car_velocity < MIN_SPEEDS[current_gear]: print('No se puede cambiar a esta marcha')
-        else:
-            if rpm < 7000: rpm += 30
-            car_velocity += rpm * rpm_to_velocity_factor * gear_ratios[current_gear] * (1 - car_velocity / MAX_SPEEDS[current_gear])
-        time = 0
-    else:
-        rpm = 0
-        if car_velocity > 0:
-            # deceleration_factor = max(0.01, car_velocity / MAX_SPEEDS[current_gear])
-            car_velocity -= min(pow(time, 2), 0.05)
-            if car_velocity < 0: car_velocity = 0
-            time += 0.001
-    print('gear ',current_gear+1, 'RPM:', rpm, 'Velocity:', car_velocity)
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    game_loop()
+                # if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    # options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
 
-    for event in pygame.event.get(): # Evento pa las keys
-        if event.type == pygame.KEYDOWN: # Cuando se preciona una tecla
-            if event.key == pygame.K_DOWN:
-                index_map = {0: 6, 2: 7, 4: 8, 6: 1, 7: 3, 8: 5}
-                if image_display.current_image_index in index_map:
-                    image_display.change_image(index_map[image_display.current_image_index])
-            elif event.key == pygame.K_RIGHT:
-                if 6 <= image_display.current_image_index < 8:
-                    image_display.change_image(image_display.current_image_index + 1)
-            elif event.key == pygame.K_LEFT:
-                if 6 < image_display.current_image_index <= 8:
-                    image_display.change_image(image_display.current_image_index - 1)
-            elif event.key == pygame.K_UP:
-                index_map = {1: 6, 3: 7, 5: 8, 6: 0, 7: 2, 8: 4}
-                if image_display.current_image_index in index_map:
-                    image_display.change_image(index_map[image_display.current_image_index])
+        pygame.display.update()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                if 6 <= image_display.current_image_index <= 8 and not pygame.key.get_pressed()[pygame.K_LEFT] and not pygame.key.get_pressed()[pygame.K_RIGHT]:
-                    image_display.change_image(7)
-            if event.key == pygame.K_ESCAPE: # Pa cerrar el juego cuando suelte el Escape
-                run = False
-
-        if event.type == pygame.QUIT:
-            run = False
-    clock.tick(60)
-   
+def game_loop():
+    screen.fill("#000000")
     
-    #pygame.display.update()
-    pygame.display.flip()
-pygame.quit()
+    # Create an instance of the ImageDisplay class
+    image_display = ImageDisplay(screen)
+    image_display.show_image()
+
+    # Car properties
+    car_velocity = 0  # Velocidad inicial del automóvil en km/h
+    current_gear = 0  # Marcha actual
+    rpm = 0  # RPM inicial
+    rpm_to_velocity_factor = 0.00007  # Factor de conversión de RPM a velocidad
+    gear_ratios = [3.5, 3, 2.5, 2, 1.5, 1]  # Relaciones de marcha para las marchas 1 a 5
+    MAX_SPEEDS = [60, 100, 140, 180, 220, 260]  # Velocidades máximas para cada marcha en km/h
+    MIN_SPEEDS = [0, 40, 80, 120, 150, 180]  # Velocidades mínimas para cada marcha en km/h
+
+    # Clock for controlling the frame rate
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        if image_display.current_image_index < 6:
+            current_gear = image_display.current_image_index
+            if car_velocity < MIN_SPEEDS[current_gear]: print('No se puede cambiar a esta marcha')
+            else:
+                if rpm < 7000: rpm += 30
+                car_velocity += rpm * rpm_to_velocity_factor * gear_ratios[current_gear] * (1 - car_velocity / MAX_SPEEDS[current_gear])
+            time = 0
+        else:
+            rpm = 0
+            if car_velocity > 0:
+                # deceleration_factor = max(0.01, car_velocity / MAX_SPEEDS[current_gear])
+                car_velocity -= min(pow(time, 2), 0.05)
+                if car_velocity < 0: car_velocity = 0
+                time += 0.001
+        print('gear ',current_gear+1, 'RPM:', rpm, 'Velocity:', car_velocity)
+
+        for event in pygame.event.get(): # Evento pa las keys
+            if event.type == pygame.KEYDOWN: # Cuando se preciona una tecla
+                if event.key == pygame.K_DOWN:
+                    index_map = {0: 6, 2: 7, 4: 8, 6: 1, 7: 3, 8: 5}
+                    if image_display.current_image_index in index_map:
+                        image_display.change_image(index_map[image_display.current_image_index])
+                elif event.key == pygame.K_RIGHT:
+                    if 6 <= image_display.current_image_index < 8:
+                        image_display.change_image(image_display.current_image_index + 1)
+                elif event.key == pygame.K_LEFT:
+                    if 6 < image_display.current_image_index <= 8:
+                        image_display.change_image(image_display.current_image_index - 1)
+                elif event.key == pygame.K_UP:
+                    index_map = {1: 6, 3: 7, 5: 8, 6: 0, 7: 2, 8: 4}
+                    if image_display.current_image_index in index_map:
+                        image_display.change_image(index_map[image_display.current_image_index])
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    if 6 <= image_display.current_image_index <= 8 and not pygame.key.get_pressed()[pygame.K_LEFT] and not pygame.key.get_pressed()[pygame.K_RIGHT]:
+                        image_display.change_image(7)
+                if event.key == pygame.K_ESCAPE: # Pa cerrar el juego cuando suelte el Escape
+                    run = False
+
+            if event.type == pygame.QUIT:
+                run = False
+        clock.tick(60)
+    
+        
+        #pygame.display.update()
+        pygame.display.flip()
+    pygame.quit()
+main_menu()
