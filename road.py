f7f26ba743be5d53d8f8da5d6321c7c3d91d151e
A00838521 <141672024+A00838521@users.noreply.github.com>
2024-03-17 15:21:34 -0600

    MENU

---
+++ road.py
@@ -0,0 +1,137 @@
+import pygame
+import os
+
+import random
+import math
+
+pygame.init()
+screen_width, screen_height = 1280, 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('CODICON')
+
+background_load = pygame.image.load('background.jpeg') #cambiar de imagen a una que no se pixelee
+background = pygame.transform.scale(background_load, (screen_width, screen_height))
+
+''''''
+def random_coeff():
+    return [random.randrange(-5,0), random.randrange(-5,5), random.randrange(-5,5), 0 ]
+
+def roadFun(pos_x, coeff):
+    return coeff[0] * (pos_x ** 3) + coeff[1] * (pos_x ** 2) + coeff[2] * pos_x + coeff[3]
+
+def der_roadFun(pos_x): #esto solo se usaria si se quiere calcular la vel max para el derrape
+    return 3 * coeff[0] * pos_x ** 2 + 2 * coeff[1] * pos_x + coeff[2]
+
+def der2_roadFun(pos_x): #esto solo se usaria si se quiere calcular la vel max para el derrape
+    return 6 * coeff[0] * pos_x + 2 * coeff[1]
+
+def calculate_roadpoints(_coeff):
+    points = []
+    i = 0
+    for x in range(-1000, 1000, 2): # TODO cortar esto para que no haya tanta linea aburrida
+        points.append((i, roadFun(x / 100, _coeff) + (screen_height / 2)))
+        i += 1
+    return points
+
+def join_road_points(points, new_points): #habrá una segunda funcion como buffer
+    offset_x = points[-1][0] - new_points[0][0]
+    offset_y = points[-1][1] - new_points[0][1]
+    for point in range(len(new_points)): #hay una manera mas bonita, solo no se me ocurrio anoche :c
+        point_list = list(new_points[point])
+        point_list[0] += offset_x
+        point_list[1] += offset_y
+        points.append(point_list)
+    return points
+
+def distancia_puntos(punto1, punto2):
+    return math.sqrt((punto1[0] - punto2[0]) ** 2 + (punto1[1] - punto2[1]) ** 2)
+
+
+def punto_mas_cercano(road_points, screen_width, screen_height):
+    #pos de tupla mas cercano al centro virtual
+    objetivo = (screen_width // 2, screen_height // 2)
+    distancia_minima = float('inf')
+    posicion_minima = None
+
+    for i, punto in enumerate(road_points):
+        distancia = distancia_puntos(punto, objetivo)
+        if distancia < distancia_minima:
+            distancia_minima = distancia
+            posicion_minima = i
+
+    return posicion_minima
+
+''''''
+#def __init__:
+road_points = calculate_roadpoints(random_coeff())
+new_road_points = calculate_roadpoints(random_coeff())
+
+speed = 10 #speed de la transmision
+
+
+mov_x = 0
+mov_y = 0
+
+
+''''''
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    #Background
+    screen.fill((255, 255, 255))
+    screen.blit(background, (0,0))
+
+    i = 0
+    while i < len(road_points):
+        x, y = road_points[i]
+        road_points[i] = (x + mov_x, y + mov_y)
+        if road_points[i][1] > 1500:
+            road_points.pop(i)
+        else:
+            i += 1
+
+    if road_points[-1][1] > -2000:
+        coeff = random_coeff()
+        new_road_points = calculate_roadpoints(coeff)
+        road_points = join_road_points(road_points, new_road_points)
+
+    #print road
+    for point in road_points:
+        pygame.draw.circle(screen, (0, 0, 0), (point), 50)  # Draw a black circle at each point
+
+    near_origin = punto_mas_cercano(road_points, screen_width, screen_height)
+    theta = math.atan( (road_points[near_origin+10][1] - road_points[near_origin][1])/ (road_points[near_origin+10][0] - road_points[near_origin][0]) )
+    vx = speed*math.cos(theta)
+    vy = speed*math.sin(theta)
+    mov_x = -vx
+    mov_y = -vy
+
+
+
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+
+    #pygame.time.wait(500 // 60)
+pygame.quit()
+
+
+
+
+
+'''
+
+ % Curvaturas
+    R(i)= (1+fp(x(i))^2)^(3/2)/abs(fpp(x(i)));
+
+    % Velocidades máximas durante la curva
+    if (x(i) > 63 && x(i) < 98) || (x(i) > 175 && x(i) < 214)
+        vmax(i) = sqrt( R(i) * 9.81 * ( (sin(peralte) + coeffFriccion*cos(peralte)) / (cos(peralte) - coeffFriccion*sin(peralte)) ) );
+    else
+        constante = ( (sin(0) + coeffFriccion*cos(0)) / (cos(0) - coeffFriccion*sin(0)) );
+        vmax(i) = sqrt( R(i) * 9.81 * constante );
+    end
+
+'''
--- transmition.py
+++ transmition.py
@@ -9,6 +9,8 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('CODICON')
 
+mode = 'medium' # Default mode
+
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/BlackHanSans-Regular.ttf", size)
 
@@ -20,12 +22,15 @@ def main_menu():
 
         MENU_TEXT = get_font(85).render("SHIFT HAPPENS", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MODE_TEXT = get_font(60).render(f"MODE: {mode.upper()}", True, "#b68f40")
+        MODE_RECT = MODE_TEXT.get_rect(center=(640, 200))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), text_input="PLAY", font=get_font(60), base_color="white", hovering_color="gray")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), text_input="OPTIONS", font=get_font(60), base_color="white", hovering_color="gray")
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 375), text_input="PLAY", font=get_font(60), base_color="white", hovering_color="gray")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(1067, 600), text_input="OPTIONS", font=get_font(60), base_color="white", hovering_color="gray")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), text_input="QUIT", font=get_font(60), base_color="white", hovering_color="gray")
 
         screen.blit(MENU_TEXT, MENU_RECT)
+        screen.blit(MODE_TEXT, MODE_RECT)
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
@@ -38,11 +43,134 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     game_loop()
-                # if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    # options()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+def options():
+    global mode
+    run = True
+    while run:
+        screen.fill("#000000")
+
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        OPTIONS_TEXT = get_font(85).render("OPTIONS", True, "#b68f40")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 100))
+
+        BACK_BUTTON = Button(image=pygame.image.load("assets/Back Rect.png"), pos=(640, 550), text_input="BACK", font=get_font(60), base_color="white", hovering_color="gray")
+        KEY_BINDINGS_BUTTON = Button(image=pygame.image.load("assets/Key Rect.png"), pos=(1067, 100), text_input="KEYS", font=get_font(60), base_color="white", hovering_color="gray")
+        EASY_BUTTON = Button(image=pygame.image.load("assets/Level Rect.png"), pos=(213, 350), text_input="EASY", font=get_font(60), base_color="#b68f40", hovering_color="#8a6d31")
+        MEDIUM_BUTTON = Button(image=pygame.image.load("assets/Level Rect.png"), pos=(640, 350), text_input="MEDIUM", font=get_font(60), base_color="#b68f40", hovering_color="#8a6d31")
+        DIFICULT_BUTTON = Button(image=pygame.image.load("assets/Level Rect.png"), pos=(1067, 350), text_input="DIFICULT", font=get_font(60), base_color="#b68f40", hovering_color="#8a6d31")
+
+
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        BACK_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        BACK_BUTTON.update(screen)
+        KEY_BINDINGS_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        KEY_BINDINGS_BUTTON.update(screen)
+        EASY_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        EASY_BUTTON.update(screen)
+        MEDIUM_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        MEDIUM_BUTTON.update(screen)
+        DIFICULT_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        DIFICULT_BUTTON.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if BACK_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    run = False
+                if KEY_BINDINGS_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    key_bindings()
+                if EASY_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    mode = 'easy'
+                    run = False
+                if MEDIUM_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    mode = 'medium'
+                    run = False
+                if DIFICULT_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    mode = 'dificult'
+                    run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+
+        pygame.display.update()
+    main_menu()
+
+def level():
+    global mode
+    if mode == 'easy':
+        aumentoRPM = 10
+    elif mode == 'medium':
+        aumentoRPM = 30
+    elif mode == 'dificult':
+        aumentoRPM = 50
+    else:
+        aumentoRPM = 30
+    return aumentoRPM
+
+def key_bindings():
+    while True:
+        screen.fill("#000000")
+
+        KEY_BINDINGS_MOUSE_POS = pygame.mouse.get_pos()
+
+        KEY_BINDINGS_TEXT = get_font(85).render("KEY BINDINGS", True, "#b68f40")
+        KEY_BINDINGS_RECT = KEY_BINDINGS_TEXT.get_rect(center=(640, 100))
+        MOVEMENT_TEXT = get_font(50).render("MOVEMENT", True, "#b68f40")
+        MOVEMENT_RECT = MOVEMENT_TEXT.get_rect(center=(250, 250))
+        BREAKS_TEXT = get_font(50).render("BREAKS!", True, "#b68f40")
+        BREAKS_RECT = BREAKS_TEXT.get_rect(center=(640, 250))
+        CLUTCH_TEXT = get_font(50).render("CLUTCH...", True, "#b68f40")
+        CLUTCH_RECT = BREAKS_TEXT.get_rect(center=(1000, 250))
+
+        BACK_BUTTON = Button(image=pygame.image.load("assets/Back Rect.png"), pos=(640, 550), text_input="BACK", font=get_font(60), base_color="white", hovering_color="gray")
+
+        screen.blit(KEY_BINDINGS_TEXT, KEY_BINDINGS_RECT)
+        screen.blit(MOVEMENT_TEXT, MOVEMENT_RECT)
+        screen.blit(BREAKS_TEXT, BREAKS_RECT)
+        screen.blit(CLUTCH_TEXT, CLUTCH_RECT)
+
+        arrows = pygame.image.load("assets/flechas.png").convert_alpha()
+        arrows = pygame.transform.scale(arrows, (196, 136))
+        screen.blit(arrows, (150, 300))
+
+        spacebar = pygame.image.load("assets/spacebar.png").convert_alpha()
+        spacebar = pygame.transform.scale(spacebar, (192, 72))
+        screen.blit(spacebar, (550, 330))
+
+        c_key = pygame.image.load("assets/c_key.png").convert_alpha()
+        c_key = pygame.transform.scale(c_key, (62, 62))
+        screen.blit(c_key, (950, 330))
+
+        BACK_BUTTON.changeColor(KEY_BINDINGS_MOUSE_POS)
+        BACK_BUTTON.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if BACK_BUTTON.checkForInput(KEY_BINDINGS_MOUSE_POS):
+                    options()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    options()
 
         pygame.display.update()
 
@@ -57,7 +185,9 @@ def game_loop():
     car_velocity = 0  # Velocidad inicial del automóvil en km/h
     current_gear = 0  # Marcha actual
     rpm = 0  # RPM inicial
-    rpm_to_velocity_factor = 0.00007  # Factor de conversión de RPM a velocidad
+    rpm_to_velocity_factor = 0.00007  # Factor de conversión de RPM a velocidad (km/h
+    aumentoRPM = level()
+    print(mode)
     gear_ratios = [3.5, 3, 2.5, 2, 1.5, 1]  # Relaciones de marcha para las marchas 1 a 5
     MAX_SPEEDS = [60, 100, 140, 180, 220, 260]  # Velocidades máximas para cada marcha en km/h
     MIN_SPEEDS = [0, 40, 80, 120, 150, 170]  # Velocidades mínimas para cada marcha en km/h
@@ -90,7 +220,7 @@ def game_loop():
             current_gear = image_display.current_image_index
             if car_velocity < MIN_SPEEDS[current_gear] or car_velocity > MAX_SPEEDS[current_gear]: print('No se puede cambiar a esta marcha')
             else:
-                if rpm < 7000: rpm += 30
+                if rpm < 7000: rpm += aumentoRPM
                 if rpm > 7000: rpm = 7000
                 if rpm == 7000:
                     maxRPMtime += 1
@@ -167,6 +297,7 @@ def game_loop():
                     if 6 <= image_display.current_image_index <= 8 and not pygame.key.get_pressed()[pygame.K_LEFT] and not pygame.key.get_pressed()[pygame.K_RIGHT] and not pygame.key.get_pressed()[pygame.K_UP] and not pygame.key.get_pressed()[pygame.K_DOWN]:
                         image_display.change_image(7)
                 if event.key == pygame.K_ESCAPE: # Pa cerrar el juego cuando suelte el Escape
+                    main_menu()
                     run = False
                 if event.key == pygame.K_SPACE:
                     breaking = False
